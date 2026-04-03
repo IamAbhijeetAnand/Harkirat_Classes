aeed7d7ae6eb0fc1afa716025d7d0f1b7491ea42
Abhijeet Anand <abhijeetanand475@gmail.com>
2026-04-03 14:16:05 +0530

    Updated Layout concepts

--- React/week-10.1/practiceSPA/src/App.jsx
+++ React/week-10.1/practiceSPA/src/App.jsx
@@ -58,29 +58,127 @@
 
 //LAYOUT CODE AND EXAMPLES BELOW
 
+// import './App.css'
+// import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div>
+//       <BrowserRouter>
+
+//         <Routes>
+//           {/* Layout Wrapper */}
+//           <Route path="/" element={<Layout />}>
+
+//             {/* Home Page */}
+//             <Route index element={<Landing />} />
+
+//             {/* Other Pages */}
+//             <Route path="neet/online-coaching-class-11" element={<Class11Program />} />
+//             <Route path="neet/online-coaching-class-12" element={<Class12Program />} />
+
+//             {/* Error Page */}
+//             <Route path="*" element={<ErrorPage />} />
+
+//           </Route>
+//         </Routes>
+
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// function Layout() {
+//   return (
+//     <div style={{ height: "100vh" }}>
+
+//       {/* Navbar */}
+//       <div>
+//         <Link to="/">Allen</Link> |{" "}
+//         <Link to="/neet/online-coaching-class-11">Class 11</Link> |{" "}
+//         <Link to="/neet/online-coaching-class-12">Class 12</Link>
+//       </div>
+
+//       {/* Main Content */}
+//       <div style={{ height: "90vh" }}>
+//         <Outlet />
+//       </div>
+
+//       {/* Footer */}
+//       <div>
+//         Footer | Contact us
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// function Landing() {
+//   return (
+//     <div>
+//       Welcome to allen
+//     </div>
+//   );
+// }
+
+// function Class11Program() {
+//   return (
+//     <div>
+//       NEET Class 11 Program
+//     </div>
+//   );
+// }
+
+// function Class12Program() {
+//   return (
+//     <div>
+//       NEET Class 12 Program
+//     </div>
+//   );
+// }
+
+// function ErrorPage() {
+//   return (
+//     <div>
+//       404 - Page Not Found
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//UPDATED CODE
+
+
 import './App.css'
-import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+
+// Import required components from react-router-dom
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
+      {/* BrowserRouter provides routing context to entire app */}
       <BrowserRouter>
 
+        {/* Routes is a container for all Route definitions */}
         <Routes>
-          {/* Layout Wrapper */}
+
+          {/* Parent Route (Layout wrapper for all pages) */}
           <Route path="/" element={<Layout />}>
 
-            {/* Home Page */}
+            {/* Index route = default page for "/" */}
             <Route index element={<Landing />} />
 
-            {/* Other Pages */}
+            {/* Child routes (render inside <Outlet /> of Layout) */}
             <Route path="neet/online-coaching-class-11" element={<Class11Program />} />
             <Route path="neet/online-coaching-class-12" element={<Class12Program />} />
 
-            {/* Error Page */}
+            {/* Wildcard route for handling invalid URLs */}
             <Route path="*" element={<ErrorPage />} />
 
           </Route>
+
         </Routes>
 
       </BrowserRouter>
@@ -90,59 +188,58 @@ function App() {
 
 function Layout() {
   return (
-    <div style={{ height: "100vh" }}>
+    // Main layout container
+    <div style={{ height: "100vh", background: "green" }}>
+      
+      {/* Common Header/Navbar */}
+      <Header />
 
-      {/* Navbar */}
-      <div>
-        <Link to="/">Allen</Link> |{" "}
-        <Link to="/neet/online-coaching-class-11">Class 11</Link> |{" "}
-        <Link to="/neet/online-coaching-class-12">Class 12</Link>
-      </div>
-
-      {/* Main Content */}
-      <div style={{ height: "90vh" }}>
+      {/* Main content area (dynamic content loads here) */}
+      <div style={{ height: "90vh", background: "red" }}>
+        {/* Outlet renders matched child route */}
         <Outlet />
       </div>
 
-      {/* Footer */}
-      <div>
+      {/* Footer (common for all pages) */}
+      <footer>
         Footer | Contact us
-      </div>
+      </footer>
 
     </div>
   );
 }
 
+function Header() {
+  return (
+    <div>
+      {/* Link is used instead of <a> to avoid page reload */}
+      <Link to="/">Allen</Link> |{" "}
+
+      {/* Navigation links */}
+      <Link to="/neet/online-coaching-class-11">Class 11</Link> |{" "}
+      <Link to="/neet/online-coaching-class-12">Class 12</Link>
+    </div>
+  );
+}
+
+// Landing (Home Page)
 function Landing() {
-  return (
-    <div>
-      Welcome to allen
-    </div>
-  );
+  return <div>Welcome to allen</div>;
 }
 
+// Class 11 Page
 function Class11Program() {
-  return (
-    <div>
-      NEET Class 11 Program
-    </div>
-  );
+  return <div>NEET Class 11 Program</div>;
 }
 
+// Class 12 Page
 function Class12Program() {
-  return (
-    <div>
-      NEET Class 12 Program
-    </div>
-  );
+  return <div>NEET Class 12 Program</div>;
 }
 
+// Error Page (for unknown routes)
 function ErrorPage() {
-  return (
-    <div>
-      404 - Page Not Found
-    </div>
-  );
+  return <div>404 - Page Not Found</div>;
 }
 
 export default App;
